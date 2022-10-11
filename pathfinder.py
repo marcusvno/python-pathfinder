88e486c8302bcfac8dd16e92b127a368b481ca91
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-10-11 00:02:30 -0400

    Map creator finished. Double check process before starting on greedy algorithm.

--- pathfinder.py
+++ pathfinder.py
@@ -7,28 +7,34 @@ def create_image(file):
 
     print(file)
     matrix = numpy.loadtxt(file)
-    rows = len(matrix)
-    cols = len(matrix[0])
+    cols = len(matrix)
+    rows = len(matrix[0])
 
-    max_elevation = int(numpy.amax(matrix))
-    min_elevation = int(numpy.amin(matrix))
+    max_elevation = numpy.amax(matrix)
+    min_elevation = numpy.amin(matrix)
+
+    """ TEST LOOPS
+    for x in range(cols):
+        for y in range(rows):
+            print(matrix[x][y]) """
 
     im = Image.new('RGBA', (rows, cols))
 
-    for x in range(rows):
-        for y in range(cols):
+    for x in range(cols):
+        for y in range(rows):
             current_color = color_calc(min_elevation, max_elevation, matrix[x][y])  # noqa
             im.putpixel((x, y), (current_color, current_color, current_color))
 
+    im.save(f'{Path(file).stem} map.png')
 
-    print(f'{Path(file).stem} has {rows} Rows and {cols}')
-    print(f'Largest number: {max_elevation} Lowest: {min_elevation}') # noqa
-
-    # im.save(f'{Path(file).stem}.png')
+    print(f'Highest Elevation: {max_elevation} Lowest: {min_elevation}') # noqa
 
 
 def color_calc(min_ele, max_ele, current_ele):
-    pass
+    percent_ele = (current_ele - min_ele) / (max_ele - min_ele)
+    color_code = int(percent_ele * 255)
+    return color_code
+
 
 if __name__ == "__main__":
     import argparse
