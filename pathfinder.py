9520d255fbce836ff32626ba7d108fdba39c6472
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-10-11 23:42:28 -0400

    Image inversion problem solved. Matrix traversal is inverted from image library's coordinate system.

--- pathfinder.py
+++ pathfinder.py
@@ -1,44 +1,32 @@
 from pathlib import Path
 
 
+def color_calc(min_ele, max_ele, current_ele):
+    percent_ele = (current_ele - min_ele) / (max_ele - min_ele)
+    color_code = int(percent_ele * 255)
+    return color_code
+
 def create_image(file):
     from PIL import Image
     import numpy as np
 
     print(f'Converting {file}...')
     matrix = np.loadtxt(file)
-    cols = len(matrix)
-    rows = len(matrix[0])
+    rows, cols = matrix.shape
 
     max_elevation = np.amax(matrix)
     min_elevation = np.amin(matrix)
 
-    # print(matrix[0][0])
-
     im = Image.new('RGBA', (rows, cols))
 
-    for x in range(cols):
-        for y in range(rows):
+    for x in range(rows):
+        for y in range(cols):
             current_color = color_calc(min_elevation, max_elevation, matrix[x][y])  # noqa
             im.putpixel((y, x), (current_color, current_color, current_color))
 
     print(f'Saving {Path(file).stem} map.png..')
     im.save(f'{Path(file).stem} map.png')
-    print('Saved!')
-
-    """TEST LOOPS
-    for x in range(5):
-        for y in range(5):
-            print(matrix[x][y])
-    
-    print(f'Highest Elevation: {max_elevation} Lowest: {min_elevation}') # noqa
-    """
-
-
-def color_calc(min_ele, max_ele, current_ele):
-    percent_ele = (current_ele - min_ele) / (max_ele - min_ele)
-    color_code = int(percent_ele * 255)
-    return color_code
+    print('...Saved!')
 
 
 if __name__ == "__main__":
